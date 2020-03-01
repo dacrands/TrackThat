a983dfebebfcd9c0cbf0ef6a300601336b748e1d
da crands <davecrands@gmail.com>
2020-03-01 11:54:23 -0500

    Add Employee model

---
+++ Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace track_that.Models
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public DateTime HireDate { get; set; }
+        public decimal Hours { get; set; }
+        
+        [Required]
+        public string UserID { get; set; }
+        public ApplicationUser User { get; set; }
+    }
+}
